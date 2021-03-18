02cae3f46ad1f9e8eed1e424b2b0847c0ab04256
Fernando Trindade <trindade-fernando@outlook.com>
2021-03-18 18:03:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Chapter II - Challenge 1
+
+<h1 align="center">
+    <img src="cover-node.js.png">
+</h1>
+
+Rocketseat Ignite - Chapter II - Challenge 1 - Introdução ao SOLID
+
+## About
+
+Creating a user registration and listing API using SOLID principles.
+
+---
+
+By Fernando Trindade 🤓
---
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+
+import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
+
+class TurnUserAdminController {
+  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    const { user_id } = request.params;
+    try {
+      const newAdminUser = this.turnUserAdminUseCase.execute({ user_id });
+      return response.json(newAdminUser);
+    } catch (err) {
+      return response.status(404).json({ error: err.message });
+    }
+  }
+}
+
+export { TurnUserAdminController };
